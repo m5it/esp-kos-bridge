97b5ede4742cab9f10afddeb464cf29978b54966
t3ch <w4d4f4k@gmail.com>
2022-12-12 17:30:47 +0000

    updates from merge

--- components/iot_bridge/src/bridge_wifi.c
+++ components/iot_bridge/src/bridge_wifi.c
@@ -53,7 +53,7 @@ esp_err_t esp_bridge_wifi_set(wifi_mode_t mode,
                               const char *ssid,
                               const char *password,
                               const char *bssid)
-//>>>>>>> master
+//git >>>>>>> master
 {
     ESP_PARAM_CHECK(ssid);
     ESP_PARAM_CHECK(password);
