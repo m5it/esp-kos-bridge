96f8d2a432469475075e746079a607cd2bd8eaac
t3ch <w4d4f4k@gmail.com>
2022-12-26 04:15:15 +0000

    added examples for pwm

--- main/t3ch_console.c
+++ main/t3ch_console.c
@@ -18,6 +18,16 @@
 #include "nvs_flash.h"
 //
 #include "driver/gpio.h"
+#include "driver/ledc.h"
+
+#define LEDC_TIMER              LEDC_TIMER_0
+#define LEDC_MODE               LEDC_LOW_SPEED_MODE
+#define LEDC_OUTPUT_IO          (26) // Define the output GPIO
+#define LEDC_CHANNEL            LEDC_CHANNEL_0
+#define LEDC_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
+#define LEDC_DUTY               (4095) // Set duty to 50%. ((2 ** 13) - 1) * 50% = 4095
+#define LEDC_FREQUENCY          (5000) // Frequency in Hertz. Set frequency at 5 kHz
+
 //--
 static const char *TAG = "T3CH_CONSOLE";
 
@@ -261,11 +271,40 @@ static struct {
 	struct arg_str *nvsWrite;
 	//
 	struct arg_lit *testTime;
+	struct arg_lit *testLedc;
+	struct arg_lit *pauseLedc;
     struct arg_int *testInt;
     struct arg_dbl *testDouble;
     struct arg_str *testString;
     struct arg_end *end;
 } test_args;
+
+//
+static void example_ledc_init(void)
+{
+    // Prepare and then apply the LEDC PWM timer configuration
+    ledc_timer_config_t ledc_timer = {
+        .speed_mode       = LEDC_MODE,
+        .timer_num        = LEDC_TIMER,
+        .duty_resolution  = LEDC_DUTY_RES,
+        .freq_hz          = LEDC_FREQUENCY,  // Set output frequency at 5 kHz
+        .clk_cfg          = LEDC_AUTO_CLK
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+
+    // Prepare and then apply the LEDC PWM channel configuration
+    ledc_channel_config_t ledc_channel = {
+        .speed_mode     = LEDC_MODE,
+        .channel        = LEDC_CHANNEL,
+        .timer_sel      = LEDC_TIMER,
+        .intr_type      = LEDC_INTR_DISABLE,
+        .gpio_num       = LEDC_OUTPUT_IO,
+        .duty           = 0, // Set duty to 0%
+        .hpoint         = 0
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+}
+
 //
 static int do_cmd_test(int argc, char **argv) {
 	ESP_LOGI(TAG,"do_cmd_test() START Hello world by test!!! :)\n");
@@ -321,6 +360,17 @@ static int do_cmd_test(int argc, char **argv) {
 		else printf("nvsWrite commit Success!\n");
 	}
     //--
+    if (test_args.testLedc->count > 0) {
+		printf("test ledc starting.");
+	    example_ledc_init();
+	    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY));
+	    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+	}
+	if (test_args.pauseLedc->count > 0) {
+		printf("pause ledc starting.");
+	    //ESP_ERROR_CHECK(ledc_timer_pause(LEDC_MODE, LEDC_TIMER));
+	    ESP_ERROR_CHECK(ledc_stop(LEDC_MODE, LEDC_CHANNEL, 0));
+	}
     if (test_args.testTime->count > 0) {
 		time_t now;
 		struct tm timeinfo;
@@ -358,6 +408,8 @@ esp_err_t register_test(void) {
 	test_args.nvsWrite   = arg_str1("w","nvsWrite","<s>","Write to nvs");
 	// default options
 	test_args.testTime   = arg_lit0("T", "testTime", "Test time");
+	test_args.testLedc   = arg_lit0("L", "testLedc", "Test ledc");
+	test_args.pauseLedc  = arg_lit0("P", "pauseLedc", "Pause ledc");
 	test_args.testInt    = arg_int0("i", "testInteger", "<n>", "Test integer argument");
 	test_args.testDouble = arg_dbl0("d", "testDouble", "<n>", "Test double argument");
 	test_args.testString = arg_str1("s", "testString", "<s>", "Test string argument");
